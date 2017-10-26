47e9db8e62d21de9902d4e64ccf6c43032fe34c2
James Dunn <jwdunn1@gmail.com>
2017-10-26 13:53:38 -0700

    Fix spelling

--- scripts/train_mobilenets.sh
+++ scripts/train_mobilenets.sh
@@ -6,7 +6,7 @@ python scripts/train_model.py \
 --validation_batch_size -1 \
 --how_many_training_steps 4000 \
 --architecture 'mobilenet_1.0_224' \
---keep_probabilities 0.5 \
+--keep_probability 0.5 \
 --learning_rate 1e-2 \
 --test_batch_size=-1 \
 --intermediate_output_graphs_dir model/checkpoints \
